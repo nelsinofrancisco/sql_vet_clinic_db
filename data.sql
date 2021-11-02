7eaa62f3855e1981236ef8ed3f5543499c19f28d
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-11-02 20:57:42 -0300

    feature: add aggregate functions and update values

--- data.sql
+++ data.sql
@@ -3,5 +3,10 @@
 INSERT INTO animals VALUES (default, 'Agumon', '20200203', 0, true, 10.23 ),
 (default, 'Gabumon', '20181115', 2, true, 8 ),
 (default, 'Pikachu', '20210107', 1, false, 15.04 ),
-(default, 'Devimon', '20170512', 5, true, 11 );
-
+(default, 'Devimon', '20170512', 5, true, 11 ),
+(default, 'Charmander', '20200208', 0, false, 11 ),
+(default, 'Plantmon', '20221115', 2, true, 5.7 ),
+(default, 'Squirtle', '19930402', 3, false, 12.13 ),
+(default, 'Angemon', '20050612', 1, true, -45 ),
+(default, 'Boarmon', '20050607', 7, true, 20.4 ),
+(default, 'Blossom', '19981013', 3, true, 17 );
