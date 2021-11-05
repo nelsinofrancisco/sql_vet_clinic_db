b33837c180f124aca177b81e6f9b549be6302ea9
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-11-05 09:43:47 -0300

    feature: add bridge tables vets and specializations

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS animals;
 DROP TABLE IF EXISTS owners;
 DROP TABLE IF EXISTS species;
+DROP TABLE IF EXISTS vets;
 
 CREATE TABLE owners (
   id INT GENERATED ALWAYS AS IDENTITY,
@@ -12,6 +13,14 @@ CREATE TABLE owners (
 CREATE TABLE species (
   id INT GENERATED ALWAYS AS IDENTITY,
   name varchar(80) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name varchar(80) NOT NULL,
+  age INT NOT NULL,
+  date_of_graduation date NOT NULL,
   PRIMARY KEY(id)
 );
 
@@ -34,3 +43,32 @@ CREATE TABLE animals (
         REFERENCES owners(id)
           ON DELETE CASCADE
 );
+
+CREATE TABLE specializations(
+  specie_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  CONSTRAINT fk_species
+    FOREIGN KEY(specie_id)
+      REFERENCES species(id)
+        ON DELETE CASCADE,
+  CONSTRAINT fk_vets
+    FOREIGN KEY(vet_id)
+      REFERENCES vets(id)
+        ON DELETE CASCADE,
+  PRIMARY KEY(specie_id, vet_id)
+)
+
+CREATE TABLE visits(
+  animal_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  CONSTRAINT fk_animals
+    FOREIGN KEY(animal_id)
+      REFERENCES animals(id)
+        ON DELETE CASCADE,
+  CONSTRAINT fk_vets
+    FOREIGN KEY(vet_id)
+      REFERENCES vets(id)
+        ON DELETE CASCADE,
+  PRIMARY KEY(animal_id, vet_id)
+);
+
